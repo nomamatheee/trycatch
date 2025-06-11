d2dfce919b151eb64016fb7e41eece5bcd618974
nomamatheee <157741578+nomamatheee@users.noreply.github.com>
2025-06-11 19:11:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+			try
+			{
+                Console.WriteLine("Please enter a number");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter another number");
+                int x = int.Parse(Console.ReadLine());
+
+                int answer = y / x;
+                Console.WriteLine(answer);
+
+
+            }
+			catch (Exception ex)
+			{
+
+                Console.WriteLine($"error " + ex.Message );
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
